7e0739867e2046a266ee01f855de3bcfff019851
Aboubacar Doucoure <yelounak@gmail.com>
2024-03-20 16:08:30 +0100

    Improved getting started

--- src/components/project/quickstart/index.js
+++ src/components/project/quickstart/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import Actual from './actual'
+import ReactView from './react'
+
+import Tabs from '@theme/Tabs'
+import TabItem from '@theme/TabItem'
+
 export default ({ hideTitle = false }) => <div class={`                     
         flex 
         justify-center`}>
@@ -10,10 +14,24 @@ export default ({ hideTitle = false }) => <div class={`
           max-w-sm
           px-4   
         `}>
-      <h2 className='text-4xl'>{`Quick start with React JS 🚀`}</h2>
+      <h2 className='text-4xl'>{`Quick start 🚀`}</h2>
       <p className={`text-lg italic`}>
             Let's build a login form with email and password inputs.
           </p>
-      <Actual />
+          <Tabs>
+        <TabItem value="React JS" label="React JS" default>
+          <ReactView />
+        </TabItem>
+        <TabItem value="React Native" label="React Native">
+            Coming soon
+        </TabItem>
+        <TabItem value="Swift" label="Swift">
+            Want to contribute? Contact me.
+        </TabItem>
+        <TabItem value="Kotlin" label="Kotlin">
+            Want to contribute? Contact me.
+        </TabItem>
+    </Tabs>
+      
     </div>
   </div>
---
+++ src/components/project/quickstart/reactnative.js
@@ -0,0 +1,105 @@
+import React from 'react'
+import CodeBlock from '@theme/CodeBlock'
+import BashTabs from '@site/src/components/generic/bashTabs'
+
+export default ({}) => <div class={`                     
+        `}>
+          <p className={`text-lg font-bold`}>
+            1. Install formulaik and a component library
+          </p>
+          <BashTabs npm={`npm install @formulaik/react-native @formulaik-community/react-native-mui`} />          
+         
+          <p className={`text-lg font-bold`}>
+          2. Define inputs   
+          </p>        
+          <CodeBlock language="jsx">
+{`const inputs = [
+  {
+    component: 'input',
+    id: 'email',
+    label: 'Email',
+    type: "string",
+    params: {
+      type: 'email',
+      placeholder: "email@domain.com"
+    },
+    validation: {
+      format: {
+        value: "email",
+        message: 'Invalid email format',
+      },
+      required: {
+        value: true,
+        message: "This field can't be blank",
+      },
+    }
+  },
+  {
+    component: 'inputPassword',
+    label: 'Password',
+    id: 'password',
+    type: "string",
+    params: {
+      type: 'password',
+      autoComplete: "current-password",
+      placeholder: "xxxx-xxxx-xxxx"
+    },
+    validation: {
+      matches: {
+        value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+        message: 'Invalid password, must contain at least 8 characters and at most 18 characters',
+      },
+      required: {
+        value: true,
+        message: "This field can't be blank",
+      },
+    }
+  },
+  {
+    component: 'submit',
+    params: {
+      text: 'Continue'
+    }
+  },
+]`}
+          </CodeBlock>
+        
+          <p className={`text-lg font-bold`}>
+            3. Provide initial values (optional)
+          </p>          
+         
+            <CodeBlock language="jsx">
+{`const values = {
+    email: cookies.get('email'),
+}`}
+            </CodeBlock>
+          <p className={`text-lg font-bold`}>
+            4. Render forms and handle submit
+          </p>
+         
+            <CodeBlock language="jsx">
+{`import Formulaik from '@formulaik/react'
+import FormulaikMui from '@formulaik-community/react-mui'
+
+export default (props) => {
+  const onSubmit = async (values, { setSubmitting }) => {
+    try {
+      const { email, password } = values
+      //... do login      
+    } catch (e) {
+      console.log(e)      
+    }
+  }
+
+  return <>      
+      <text>Login</text>            
+      <Formulaik
+        components={[FormulaikLocal]}
+        values={values}        
+        inputs={inputs}
+        onSubmit={onSubmit}
+         />      
+    </>
+}`}
+      </CodeBlock>        
+    </div>
