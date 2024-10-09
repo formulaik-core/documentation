06bbb33a8bd2d44469ca650dd9b92b25261dbf90
Aboubacar Doucoure <yelounak@gmail.com>
2024-10-09 21:14:57 +0200

    fix: updated home

--- src/components/project/homepageFeatures2/featureList.js
+++ src/components/project/homepageFeatures2/featureList.js
@@ -6,25 +6,25 @@ export default [
     Svg: require('@site/static/img/noun-strategic-3754842.svg').default,
     description: (
       <>
-        One of the fundamental principles of React is the concept of componentization. Apply this principle to your forms by breaking them down into small, reusable components.
+        Formulaik's architecture helps you define your input components in separated files and register them in a component library.  
       </>
     ),
   },
   {
-    title: 'Yup validation',
+    title: 'Declare once, use everywhere',
     Svg: require('@site/static/img/noun-check-3754833.svg').default,
     description: (
       <>
-        Yup is a schema builder for runtime value parsing and validation. Define a schema, transform a value to match, assert the shape of an existing value, or both. Yup schema are extremely expressive and allow modeling complex, interdependent validations, or value transformation.
+        Formulaik inputs are pure JSON so that the component they use, the validations and the customizations are easy to read, update and maintain. 
       </>
     ),
   },
   {
-    title: 'Formik underneath',
+    title: 'Multi-platform',
     Svg: require('@site/static/img/formik.svg').default,
     description: (
       <>
-        On React we use the powerful Formik library for rendering the inputs. Formik is the world's most popular open source form library for React and React Native.
+        On React and React-native we use the powerful Formik library for rendering the inputs. We look forward to making Formulaik available on other major platforms.
       </>
     ),
   },
